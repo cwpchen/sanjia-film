12457ace61e2cb30bb26ffd652be4038986f7473
park <17311206115@163.com>
2019-04-28 21:38:51 +0800

    add interceptor

--- sanjiaFilm/sanjia-common/src/main/java/com/sj/common/config/UrlAddr.java
+++ sanjiaFilm/sanjia-common/src/main/java/com/sj/common/config/UrlAddr.java
@@ -5,6 +5,7 @@ public class UrlAddr {
 	public final static String userRegisterKey01 = "/checkUserName/";
 	public final static String userRegisterKey02 = "/save/";
 	public final static String userLoginKey03 = "/login/";
+	public final static String userLoginKey04 = "/query/";
 	
 	public final static String userLoginUrl="user.sj.com/user/";
 }
